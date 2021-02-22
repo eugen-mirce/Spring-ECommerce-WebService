eef5a7987a9cbe80f93f9714a6778387a2becffe
Eugen Mirce <40607123+eugenm92@users.noreply.github.com>
2021-02-22 17:54:46 +0100

    Added custom exceptions

--- src/main/java/com/project/app/controller/UserController.java
+++ src/main/java/com/project/app/controller/UserController.java
@@ -2,10 +2,13 @@ package com.project.app.controller;
 
 import com.project.app.shared.dto.UserDto;
 import com.project.app.ui.model.request.UserDetailsRequestModel;
+import com.project.app.ui.model.response.ErrorMessages;
 import com.project.app.ui.model.response.UserRest;
+import com.project.app.ws.exceptions.UserServiceException;
 import com.project.app.ws.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +17,10 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path="/{id}")
+    @GetMapping(
+            path="/{id}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+            )
     public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
 
@@ -23,9 +29,14 @@ public class UserController {
 
         return returnValue;
     }
-    @PostMapping
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+            )
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws UserServiceException {
         UserRest returnValue = new UserRest();
+
+        if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails, userDto);
 
