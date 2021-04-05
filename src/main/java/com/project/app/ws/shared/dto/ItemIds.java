a7db0c669304a0b3061682a729563b9237614ae5
Eugen Mirce <40607123+eugenm92@users.noreply.github.com>
2021-04-05 22:53:14 +0200

    Added Set Of Products In Products

---
+++ src/main/java/com/project/app/ws/shared/dto/ItemIds.java
@@ -0,0 +1,12 @@
+package com.project.app.ws.shared.dto;
+
+public class ItemIds {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+}
